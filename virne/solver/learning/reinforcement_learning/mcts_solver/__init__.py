08fd855f74560e82440196f6d69e3c6d247e540b
Stephen Reilly <stephenmichaelreilly@gmail.com>
2025-07-18 03:10:17 -0700

    refactor: split AlphaZero solver

--- virne/solver/learning/reinforcement_learning/mcts_solver/__init__.py
+++ virne/solver/learning/reinforcement_learning/mcts_solver/__init__.py
@@ -1,7 +1,11 @@
 from .mcts import MctsSolver
 from .alpha_zero_sfc_solver import AlphaZeroSFCSolver
+from .actor import AlphaZeroActor
+from .learner import AlphaZeroLearner
 
 __all__ = [
     'MctsSolver',
     'AlphaZeroSFCSolver',
+    'AlphaZeroActor',
+    'AlphaZeroLearner',
 ]
